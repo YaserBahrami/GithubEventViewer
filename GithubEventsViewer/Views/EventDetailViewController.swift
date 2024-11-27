475e6e4bccc54bb9bca71015c940f08f77d50ebc
Yaser Bahrami <yaserbahrami@outlook.com>
2024-11-27 11:26:42 +0300

    change in event detail UI

--- GithubEventsViewer/Views/EventDetailViewController.swift
+++ GithubEventsViewer/Views/EventDetailViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 import SnapKit
 
 class EventDetailViewController: UIViewController {
-
+    
     private let event: GitHubEvent
+    
+    private let eventTypeLabel = UILabel()
+    private let eventRepoUrlLabel = UILabel()
+    private let repoNameLabel = UILabel()
+    private let actorNameLabel = UILabel()
+    private let avatarImageView = UIImageView()
     
     init(event: GitHubEvent) {
         self.event = event
@@ -20,55 +26,83 @@ class EventDetailViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
+        configureUI()
     }
     
     private func setupUI() {
+        // Add labels and image views to the view
         view.backgroundColor = .systemBackground
-        let titleLabel = UILabel()
-        titleLabel.text = "Event Type: \(event.type)"
-        titleLabel.textAlignment = .center
         
-        let repoLabel = UILabel()
-        repoLabel.text = "Repository: \(event.repo.name)"
-        repoLabel.textAlignment = .center
-        
-        let avatarImageView = UIImageView()
-        avatarImageView.contentMode = .scaleAspectFill
-        avatarImageView.clipsToBounds = true
-        
-        if let url = URL(string: event.actor.avatarUrl) {
-            URLSession.shared.dataTask(with: url) { data, _, _ in
-                guard let data = data else { return }
-                DispatchQueue.main.async {
-                    avatarImageView.image = UIImage(data: data)
-                }
-            }.resume()
-        }
-        
-        view.addSubview(titleLabel)
-        view.addSubview(repoLabel)
+        // Add views
         view.addSubview(avatarImageView)
+        view.addSubview(actorNameLabel)
+        view.addSubview(eventTypeLabel)
+        view.addSubview(eventRepoUrlLabel)
+        view.addSubview(repoNameLabel)
         
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
-            make.leading.trailing.equalToSuperview().inset(16)
-        }
         
-        repoLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview().inset(16)
-        }
         
+        // Constraints for avatar image
         avatarImageView.snp.makeConstraints { make in
-            make.top.equalTo(repoLabel.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
-            make.width.height.equalTo(100)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left).inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.width.height.equalTo(80)
+        }
+        
+        // Constraints for actor name
+        actorNameLabel.snp.makeConstraints { make in
+            make.left.equalTo(avatarImageView.snp.right).offset(10)
+            make.top.equalTo(avatarImageView.snp.top)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).inset(20)
+        }
+        
+        // Constraints for event type label
+        eventTypeLabel.snp.makeConstraints { make in
+            make.left.equalTo(actorNameLabel.snp.left)
+            make.top.equalTo(actorNameLabel.snp.bottom).offset(10)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).inset(20)
+
+            
+        }
+        
+        // Constraints for repo name label
+        repoNameLabel.snp.makeConstraints { make in
+            make.left.equalTo(eventTypeLabel.snp.left)
+            make.top.equalTo(eventTypeLabel.snp.bottom).offset(10)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).inset(20)
+            make.height.greaterThanOrEqualTo(30)
+            
+        }
+        
+        repoNameLabel.numberOfLines = -1
+        
+        // Constraints for event description label
+        eventRepoUrlLabel.snp.makeConstraints { make in
+            make.left.equalTo(repoNameLabel.snp.left)
+            make.top.equalTo(repoNameLabel.snp.bottom).offset(10)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).inset(20)
+
+            make.height.greaterThanOrEqualTo(30)
+        }
+        eventRepoUrlLabel.numberOfLines = -1
+        
+    }
+    
+    private func configureUI() {
+        // Set the data from the event
+        eventTypeLabel.text = "Event Type: \(event.type)"
+        eventRepoUrlLabel.text = "Repo Url: \(event.repo.url)"
+        repoNameLabel.text = "Repo: \(event.repo.name)"
+        actorNameLabel.text = "Actor: \(event.actor.login)"
+        
+        // Load avatar image asynchronously
+        if let url = URL(string: event.actor.avatarUrl) {
+            avatarImageView.loadImage(from: url)
         }
     }
-
 }
